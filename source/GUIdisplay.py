852a63fee584487f5426cea171a6dfaa7c912dc6
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-12-02 22:23:21 +0700

    them

---
+++ source/GUIdisplay.py
@@ -0,0 +1,39 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
+import os
+
+
+class Ui(QtWidgets.QMainWindow):
+    """
+    Application
+    """
+
+    def __init__(self, main_path):
+        super(Ui, self).__init__()
+        self.main_path = main_path
+        # load GUI
+        gui_path = os.path.abspath(
+            os.path.join(main_path, "GUI", "GUIdisplay.ui"))
+        uic.loadUi(gui_path, self)
+        #self.setup_pushButton()
+        self.show()
+        
+    def setup_pushButton(self):
+        # GB_informatin_custom QGroupBox
+        BT_exit: QPushButton = self.findChild(QPushButton, "BT_exit")
+        BT_exit.clicked.connect(self.BT_quit_click)
+        
+    def BT_quit_click(self):
+        window.close()
+        
+    
+
+if __name__ == "__main__":
+    path = os.path.abspath(os.path.dirname(__file__))
+    main_path = os.path.abspath(os.path.join(path, os.pardir))
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui(main_path=main_path)
+    app.exec_()
