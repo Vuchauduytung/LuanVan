5b7e67e81c49ea643ad88bbe8c93498009d688ea
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-12-03 11:27:03 +0700

    123

--- source/GUIsearch.py
+++ source/GUIsearch.py
@@ -4,6 +4,9 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import sys
 import os
+import numpy as np
+# Import json module
+from modules.library.IO_support import *
 
 
 class Ui(QtWidgets.QMainWindow):
@@ -19,9 +22,11 @@ class Ui(QtWidgets.QMainWindow):
             os.path.join(main_path, "GUI", "GUIsearch.ui"))
         uic.loadUi(gui_path, self)
         self.setup_lineEdit()
+        self.setup_foreseen_children()
         self.setup_pushButton_search()
         self.setup_pushButton_exit()
         self.show()
+
         
     def setup_lineEdit(self):
         LE_name_custom: QLineEdit = self.findChild(QLineEdit, "LE_name_custom")
@@ -60,6 +65,21 @@ class Ui(QtWidgets.QMainWindow):
             self.action.setVisible(False)
         QLineEdit.focusOutEvent(self, event)
         
+    def setup_foreseen_children(self):
+        LT_informatin_custom: QGridLayout = self.findChild(QGridLayout, "LT_informatin_custom")
+        GB_informatin_custom: QGroupBox = self.findChild(QGroupBox, "GB_informatin_custom")
+        L_Warning = QLabel(GB_informatin_custom)
+        GB_informatin_custom.label_warning = L_Warning
+        L_Warning.setText("Không có dữ liệu")
+        LT_informatin_custom.addWidget(L_Warning,
+                            4,
+                            0,
+                            1,
+                            2,
+                            Qt.AlignCenter)
+        L_Warning.setStyleSheet("QLabel { color : red; }")
+        L_Warning.setVisible(False)
+        
     def setup_pushButton_search(self):
         # GB_informatin_custom QGroupBox
         BT_search: QPushButton = self.findChild(QPushButton, "BT_search")
@@ -67,12 +87,27 @@ class Ui(QtWidgets.QMainWindow):
         
     def setup_pushButton_exit(self):
         # GB_informatin_custom QGroupBox
-        BT_exit: QPushButton = self.findChild(QPushButton, "BT_exit")
-        BT_exit.clicked.connect(self.BT_quit_click)
+        BT_cancel: QPushButton = self.findChild(QPushButton, "BT_cancel")
+        BT_cancel.clicked.connect(self.BT_quit_click)
         
     def BT_search_click(self):
-        window.close()
-    
+        LE_name_custom: QLineEdit = self.findChild(QLineEdit, "LE_name_custom")
+        LE_phone_number: QLineEdit = self.findChild(QLineEdit, "LE_phone_number")
+        LE_number_car: QLineEdit = self.findChild(QLineEdit, "LE_number_car")
+        value = LE_phone_number
+        #Open json
+        data_path = os.path.abspath(os.path.join(self.main_path, "source", "data.json"))
+        customer = json2dict(data_path)
+        keyvalue = 'phone'
+        # phone tra trong json
+        if customer[keyvalue] == LE_phone_number.text():
+            file_data = os.path.abspath(os.path.join(self.main_path, "source", "GUIdisplay.py"))
+            os.system('python "{}"'.format(file_data))
+        else :
+            GB_informatin_custom: QGroupBox = self.findChild(QGroupBox, "GB_informatin_custom")
+            GB_informatin_custom.label_warning.setVisible(True)
+            
+            
     def BT_quit_click(self):
         window.close()
         
