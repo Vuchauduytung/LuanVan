96483f867de1bb8abe5d3c28656c36cdce961b55
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-12-04 20:33:56 +0700

    sua

--- source/GUIsearch.py
+++ source/GUIsearch.py
@@ -108,7 +108,6 @@ class Ui(QtWidgets.QMainWindow):
             GB_informatin_custom: QGroupBox = self.findChild(QGroupBox, "GB_informatin_custom")
             GB_informatin_custom.label_warning.setVisible(True)
             
-        window.close()
             
     def BT_quit_click(self):
         window.close()
