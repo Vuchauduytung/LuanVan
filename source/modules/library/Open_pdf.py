46ead8c72d92d971cca1d9304f8502e3e5ca025e
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-12-08 19:34:05 +0700

    Update Open pdf

--- source/modules/library/Open_pdf.py
+++ source/modules/library/Open_pdf.py
@@ -8,19 +8,19 @@ if 0.8*compression_pressure < Pmax < compression_pressure*1.1 and 0.8*Minimum_pr
 else:
     value = 'Hư hỏng'
     path_open = "F:\HK211\Luận Văn\source\library\libary_fix\Tháo lắp động cơ.pdf"
-    if Pci < Minimum_pressure:
+    if Pmax < Minimum_pressure:
         path = 'F:\HK211\Luận Văn\source\library\libary_fix\Gãy xéc măng, gãy xupap lủng piston.pdf'
-    elif Minimum_pressure < Pci <0.62*compression_pressure:
+    elif Minimum_pressure < Pmax <0.62*compression_pressure:
         path = 'F:\HK211\Luận Văn\source\library\libary_fix\Hở giăng nắp máy.pdf'
-    elif 0.62*compression_pressure < Pci <0.8*compression_pressure:
+    elif 0.62*compression_pressure < Pmax <0.8*compression_pressure:
         path = 'F:\HK211\Luận Văn\source\library\libary_fix\Hở xupap.pdf'
-    elif Pci > compression_pressure:
+    elif Pmax > compression_pressure:
         path = 'F:\HK211\Luận Văn\source\library\libary_fix\Xilanh không đều.pdf'
-    elif Pci > 0.9*compression_pressure:
+    elif Pmax > 0.9*compression_pressure:
         path = 'F:\HK211\Luận Văn\source\library\libary_fix\Xilanh không đều.pdf'
     elif Pmin < 0.8*Minimum_pressure_intake:
         path = 'F:\HK211\Luận Văn\source\library\libary_fix\Hở xupap.pdf'
-    elif Pmin > 0.62*P_iMinimum_pressure_intakentake:
+    elif Pmin > 0.62*Minimum_pressure_intake:
         path = 'F:\HK211\Luận Văn\source\library\libary_fix\Hở giăng nắp máy.pdf'
 
 webbrowser.open_new(path_open)
