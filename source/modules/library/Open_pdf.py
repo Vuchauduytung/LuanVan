8aa0815df578960cdae2eabcd760bb4a4bd17d96
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-12-21 17:09:28 +0700

    sua

--- source/modules/library/Open_pdf.py
+++ source/modules/library/Open_pdf.py
@@ -1,7 +1,7 @@
 # Method 3: Open with webbrowser
 
-def open_c(compression_pressure, Pmax, Pmin, Minimum_pressure_intake, minimum_pressure ):
-        # Pmax lay gia ti lon nhat trong .dat , Pmin lay gia trinho nhat trong .dat
+def open_c(compression_pressure, Pmax, P_in, Minimum_pressure_intake, minimum_pressure ):
+        # Pmax lay gia ti lon nhat trong .dat , P_in lay gia trinho nhat trong .dat
     if 0.9*compression_pressure < Pmax < compression_pressure*1.1:
         value = 'Bình thường'
         path_open = "0"
@@ -21,19 +21,19 @@ def open_c(compression_pressure, Pmax, Pmin, Minimum_pressure_intake, minimum_pr
             path = 'source\library\libary_fix\Xilanh không đều.pdf'
     return  value,path_open,path
 
-def open_in(compression_pressure, Pmax, Pmin, Minimum_pressure_intake, Minimum_pressure_charge ):
-        # Pmax lay gia ti lon nhat trong .dat , Pmin lay gia trinho nhat trong .dat
-    if 0.8*Minimum_pressure_intake < Pmin < Minimum_pressure_intake*1.1:
+def open_in(compression_pressure, Pmax, P_in, Minimum_pressure_intake, Minimum_pressure_charge ):
+        # Pmax lay gia ti lon nhat trong .dat , P_in lay gia trinho nhat trong .dat
+    if 0.8*Minimum_pressure_intake < P_in < Minimum_pressure_intake*1.1:
         value_in = 'Bình thường'
         path_open_in = "0"
         path_in = "0"
     else:
         value_in = 'Hư hỏng'
         path_open_in = "source\library\libary_fix\Tháo lắp động cơ.pdf"
-        if 0.62*Minimum_pressure_charge < Pmin < 0.8*Minimum_pressure_charge:
+        if 0.62*Minimum_pressure_charge < P_in < 0.8*Minimum_pressure_charge:
             path_in = 'source\library\libary_fix\Hở xupap.pdf'
-        elif Pmin < 0.62*Minimum_pressure_charge:
+        elif P_in < 0.62*Minimum_pressure_charge:
             path_in = 'source\library\libary_fix\Lọt khí xec măng.pdf'
-        elif Pmin < Minimum_pressure_charge:
+        elif P_in < Minimum_pressure_charge:
             path_in = 'source\library\libary_fix\Hư hỏng xylanh.pdf'
     return  value_in,path_open_in,path_in
