80cfe73e4e5e5abb7ba0e1ea54f3f8cbe0ac50ba
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-12-27 13:31:25 +0700

    Update new

--- Program/Dulieukhachhang.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-path = os.path.abspath(os.path.dirname(__file__))
-main_path = os.path.abspath(os.path.join(path, os.pardir))
-direct_path = os.path.abspath(os.path.join(main_path, "source", "GUIinput.py"))
-os.system("python3 \"{path}\""\
-    .format(path=direct_path))
